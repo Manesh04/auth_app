a836f4faf4ce42aed73e74a58e55dac123709e57
manish <manisht@fin-techsolutions.com>
2026-01-10 00:56:18 +0530

    Implemented Production-Ready API Docs for Spring Boot Auth App[Swagger + Security]

---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/config/AppConstant.java
@@ -0,0 +1,11 @@
+package com.substring.auth.auth_app_backend.config;
+
+public class AppConstant {
+
+    public static final String[] AUTH_PUBLIC_URL={
+            "/api/v1/auth/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**"
+    };
+}
--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/config/SecurityConfig.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/config/SecurityConfig.java
@@ -44,10 +44,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(authorizeHttpRequests ->
                         authorizeHttpRequests
-                                .requestMatchers("/api/v1/auth/register").permitAll()
-                                .requestMatchers("/api/v1/auth/login").permitAll()
-                                .requestMatchers("/api/v1/auth/refresh").permitAll()
-                                .requestMatchers("/api/v1/auth/logout").permitAll()
+                                .requestMatchers(AppConstant.AUTH_PUBLIC_URL).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2->
